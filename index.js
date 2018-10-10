667f894f53ce90e44ce166cd57995456498bbe44
btoner <ben.toner@wework.com>
2018-10-10 17:27:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(drivers, driver){
-  return drivers.filter(name => driver == name.toLowerCase())
+  return drivers.filter(name => driver.toLowerCase() == name.toLowerCase())
   
 }
