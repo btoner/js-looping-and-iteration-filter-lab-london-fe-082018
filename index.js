f67281c9c9a411ce34d2be367e8f35a4a664bc7b
btoner <ben.toner@wework.com>
2018-10-10 17:31:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
-function findMatching(drivers, driver){
-  return drivers.filter(name => driver.toLowerCase() == name.toLowerCase())
+function findMatching(drivers, driverLowerCase){
+  return drivers.filter(name => driverLowerCase.toLowerCase() == name.toLowerCase())
   }
   
 // function fuzzyMatch(drivers, )
