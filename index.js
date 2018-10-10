210fc8e51a2522daa463a8893530acccc62e5341
btoner <ben.toner@wework.com>
2018-10-10 17:59:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function findMatching(drivers, driverLowerCase){
   
 function fuzzyMatch(drivers, driverMatch){
   return drivers.filter(name => driverMatch.startsWith('Sa') == name.startsWith('Sa'))
-  return drivers.filter(name => driverMatch.substr()
+  return drivers.filter(name => driverMatch.substr(drivers, )
 }
 
 //function matchName(drivers, driverMatch){
